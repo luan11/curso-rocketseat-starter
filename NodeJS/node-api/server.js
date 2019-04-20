8b05aa3f98fcac9c382268b6e043f8445c2cc9c6
Luan Novais <luaannovais@live.com>
2019-04-20 14:59:37 -0300

    Curso de node.js finalizado, basicamente introduzindo a criação de apis com node

--- NodeJS/node-api/server.js
+++ NodeJS/node-api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o APP
 const app = express();
 app.use(express.json());
+app.use(cors()); // Ver documentação para poder limitar domínios que podem acessar nossa API
 
 // Iniciando o DB
 mongoose.connect(
